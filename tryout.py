dc3fbc91fa4b29f38e4b77fc2adf9634b7c46f97
kaleabsolomon-00 <kaleabsolomon634@gmail.com>
2025-12-07 11:39:00 +0000

    started to make a encryption and decryption program

---
+++ tryout.py
@@ -0,0 +1,21 @@
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+def encrypt(val, shift):
+
+def decrypt(val, shift):
+
+while True:
+    choice=input("a.encrypt\nb.decrypt\n")
+    if choice == "a" or choice== "A":
+        val=input(" enter the word:  ")
+        shift=input(" enter the shifting position:  ")
+        encrypt(val,shift)
+    elif choice == "b" or choice== "B":
+        val = input(" enter the :  ")
+        shift = input(" enter the shifting position:  ")
+        decrypt(val, shift)
+    else:
+        print("wrong choice try again")
